a7104b3600fc8163fb3cae971d6ede1e9b04506d
weiwenhao <1101140857@qq.com>
2021-09-09 01:24:30 +0800

    feat: assembler processing...

---
+++ src/assembler/amd64/asm.c
@@ -0,0 +1,5 @@
+//
+// Created by weiwenhao on 2021/9/7.
+//
+
+#include "asm.h"
---
+++ src/assembler/amd64/asm.h
@@ -0,0 +1,22 @@
+#ifndef NATURE_SRC_ASSEMBLER_AMD64_ASM_H_
+#define NATURE_SRC_ASSEMBLER_AMD64_ASM_H_
+
+// 头结构 header
+
+// 段表结构
+
+// ELF 符号表
+
+// 字符串表
+
+// 段表
+
+// 段表字符串表
+
+// 代码段(16进制)
+
+// 数据段
+
+
+
+#endif //NATURE_SRC_ASSEMBLER_AMD64_ASM_H_
