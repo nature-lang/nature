7d6074c077270e26d4ed06252e75fdbcde9153e1
weiwenhao <1101140857@qq.com>
2022-03-23 19:28:19 +0800

    feat: elf build processing...

---
+++ src/assembler/elf/elf.c
@@ -0,0 +1,16 @@
+#include "elf.h"
+
+/**
+ *
+ * 输入：text 代码段 (byte 列表)
+ * .data 变量定义段是否需要？
+ */
+void elf_target_build() {
+  // elf header 文件头表构建
+
+  // .text 代码段
+
+  // 重定位表
+
+  // section header table 段表构建
+}
