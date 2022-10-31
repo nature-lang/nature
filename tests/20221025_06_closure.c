285e9c0e5eb2fe4fbca4e362aa42ec6de63de03b
weiwenhao <1101140857@qq.com>
2022-11-01 01:07:41 +0800

    chore: test closure processing...

---
+++ tests/20221025_06_closure.c
@@ -0,0 +1,15 @@
+#include "test.h"
+#include "utils/assertf.h"
+#include "utils/exec.h"
+#include <stdio.h>
+
+static void test_basic() {
+    char *raw = exec_output();
+
+    assert_string_equal(raw, "0\n1\n2\n3\n4\n5\n6\n7\n8\n9\n10\n11\n12\n13\n14\n15\n16\n17\n18\n19\n"
+                             "while end, i=19");
+}
+
+int main(void) {
+    TEST_BASIC
+}
