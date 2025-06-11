d5fc9c963ca26115f49fff62a19fdf89f58ba569
weiwenhao <1101140857@qq.com>
2025-06-12 00:44:26 +0800

    feat: the libc library adds stdlib.h related functions

--- runtime/nutils/string.c
+++ runtime/nutils/string.c
@@ -33,40 +33,6 @@ n_string_t *string_new(void *raw_string, int64_t length) {
     return str;
 }
 
-n_string_t *rt_string_ref_new(void *raw_string, int64_t length) {
-    n_string_t *str = rti_gc_malloc(string_rtype.size, &string_ref_rtype);
-    str->data = raw_string;// 直接指向 raw_string, 而没有创建新的字符串
-    str->length = length;
-    str->capacity = length;
-    str->element_size = rtype_stack_size(&string_element_rtype, POINTER_SIZE);
-    str->hash = string_rtype.hash;
-
-    DEBUGF("[rt_string_ref_new] create, str: %p", raw_string);
-    return str;
-}
-
-/**
- * c 语言字符串中添加 '\0' 作为结束字符
- * @param n_str
- * @return
- */
-void *rt_string_ref(n_string_t *n_str) {
-
-    DEBUGF("[rt_string_ref] length=%lu, data=%p", n_str->length, n_str->data);
-
-    // 空间足够，且最后一位已经是 0， 可以直接返回
-    if (n_str->capacity > n_str->length && n_str->data[n_str->length] == '\0') {
-        return n_str->data;
-    }
-
-    // 结尾添加 '\0' 字符
-    int a = '\0';
-    rt_vec_push(n_str, string_element_rtype.hash, &a);
-    n_str->length -= 1;
-
-    return n_str->data;
-}
-
 n_string_t *string_concat(n_string_t *a, n_string_t *b) {
 
     DEBUGF("[runtime.string_concat] a=%s, b=%s", a->data, b->data);
