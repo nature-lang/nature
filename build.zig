d5cb3d41efd3e78784bfda8a63bf445c2473eb90
dcyun <Dominic_Duan@163.com>
2026-07-30 23:14:55 +0800

    update zig version to 0.17.0-dev

--- build.zig
+++ build.zig
@@ -5,6 +5,10 @@ pub fn build(b: *std.Build) !void {
 
     const optimize = b.standardOptimizeOption(.{});
 
+    var threaded = std.Io.Threaded.init(std.heap.page_allocator, .{});
+    defer threaded.deinit();
+    const io = threaded.io();
+
     // const os = target.result.os.tag;
     // const arch = target.result.cpu.arch;
     const exe = b.addExecutable(.{
@@ -19,19 +23,21 @@ pub fn build(b: *std.Build) !void {
         .include_path = "config/config.h",
     }, .{
         .BUILD_VERSION = blk: {
-            const file = try std.fs.cwd().openFile("VERSION", .{});
-            defer file.close();
-            const version_content = try file.readToEndAlloc(b.allocator, 1024);
-            break :blk std.mem.trim(u8, version_content, "\n ");
+            const file = try std.Io.Dir.cwd().openFile(io, "./VERSION", .{});
+            defer file.close(io);
+            var file_buffer: [7]u8 = undefined;
+            const n = try file.readPositionalAll(io, &file_buffer, 0);
+
+            break :blk file_buffer[0..n];
         },
-        .BUILD_TIME = std.time.timestamp(),
+        // TODO zig std time already update,build_time need afresh realize.
+        .BUILD_TIME = 0,
         .BUILD_TYPE = if (optimize == .Debug) "debug" else "release",
     });
-    std.debug.print("std.time.timestamp(): {}", .{std.time.timestamp()});
-    exe.addConfigHeader(config_header);
 
-    exe.addIncludePath(.{ .cwd_relative = "./" });
-    exe.addIncludePath(.{ .cwd_relative = "./include/" });
+    exe.root_module.addConfigHeader(config_header);
+    exe.root_module.addIncludePath(.{ .cwd_relative = "./" });
+    exe.root_module.addIncludePath(.{ .cwd_relative = "./include/" });
 
     setCMacros(exe, target);
 
@@ -39,7 +45,7 @@ pub fn build(b: *std.Build) !void {
     defer {
         for (nature_utils.items) |path| b.allocator.free(path);
     }
-    exe.addCSourceFiles(.{ .files = nature_utils.items, .flags = &.{"-std=gnu11"} });
+    exe.root_module.addCSourceFiles(.{ .files = nature_utils.items, .flags = &.{"-std=gnu11"} });
 
     const nature_main = &.{"main.c"};
     const nature_src = try findCFiles(b.allocator, "src");
@@ -47,25 +53,20 @@ pub fn build(b: *std.Build) !void {
         for (nature_src.items) |path| b.allocator.free(path);
     }
 
-    exe.addCSourceFiles(.{ .files = nature_main, .flags = &.{"-std=gnu11"} });
-    exe.addCSourceFiles(.{ .files = nature_src.items, .flags = &.{} });
+    exe.root_module.addCSourceFiles(.{ .files = nature_main, .flags = &.{"-std=gnu11"} });
+    exe.root_module.addCSourceFiles(.{ .files = nature_src.items, .flags = &.{} });
 
     b.installArtifact(exe);
 
     const run_cmd = b.addRunArtifact(exe);
-
     run_cmd.step.dependOn(b.getInstallStep());
-
-    if (b.args) |args| {
-        run_cmd.addArgs(args);
-    }
 
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 }
 
 pub fn findCFiles(allocator: std.mem.Allocator, dir_path: []const u8) !std.ArrayList([]const u8) {
-    var result = std.ArrayList([]const u8){};
+    var result = std.ArrayList([]const u8).empty;
     errdefer result.deinit(allocator);
 
     try recursiveFindCFiles(allocator, dir_path, &result);
@@ -77,14 +78,17 @@ fn recursiveFindCFiles(
     base_dir: []const u8,
     result: *std.ArrayList([]const u8),
 ) !void {
-    var dir = std.fs.cwd().openDir(base_dir, .{ .iterate = true }) catch |err| {
+    var threaded = std.Io.Threaded.init(std.heap.page_allocator, .{});
+    defer threaded.deinit();
+    const io = threaded.io();
+    var dir = std.Io.Dir.cwd().openDir(io, base_dir, .{ .iterate = true }) catch |err| {
         std.log.debug(" Failed to open directory '{s}': {s}", .{ base_dir, @errorName(err) });
         return;
     };
-    defer dir.close();
+    defer dir.close(io);
 
     var iter = dir.iterate();
-    while (try iter.next()) |entry| {
+    while (try iter.next(io)) |entry| {
         const full_path = try std.fs.path.join(allocator, &.{ base_dir, entry.name });
         defer allocator.free(full_path);
 
@@ -112,6 +116,9 @@ fn setCMacros(compile: *std.Build.Step.Compile, target: std.Build.ResolvedTarget
         .linux => {
             compile.root_module.addCMacro("__LINUX", "1");
             compile.root_module.addCMacro("_GNU_SOURCE", "1");
+        },
+        .windows => {
+            compile.root_module.addCMacro("__WINDOWS", "1");
         },
         else => {},
     }
